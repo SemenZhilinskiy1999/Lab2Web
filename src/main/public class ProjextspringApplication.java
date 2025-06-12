2f2dd9b6070cd1d9173df8df0d474b751b8e6f9b
SemenZhilinskiy1999 <99233461+SemenZhilinskiy1999@users.noreply.github.com>
2025-06-12 13:48:08 +0700

    Create public class ProjextspringApplication.java

---
+++ src/main/public class ProjextspringApplication.java
@@ -0,0 +1,13 @@
+package com.project.projextspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjextspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjextspringApplication.class, args);
+	}
+
+}
