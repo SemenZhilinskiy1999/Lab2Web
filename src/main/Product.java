84243cab6a917d88c67f12804057d5e369168f1d
SemenZhilinskiy1999 <99233461+SemenZhilinskiy1999@users.noreply.github.com>
2025-06-12 13:49:18 +0700

    Create Product.java

---
+++ src/main/Product.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+public class Product {
+    private String id;
+    private String name;
+    private int calories;
+
+    public Product() {
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+}
+
